c4aa95ddcd99a1743de0ee8b60ffb9d692392137
bobon4uto <vovapetrenko2101@gmail.com>
2026-06-26 00:15:00 +0700

    works

---
+++ lua/cheader/init.lua
@@ -0,0 +1,34 @@
+local PLUGIN_NAME = "cheader"
+
+local function insert(text)
+  local buf = 0
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  col = col + 1
+  vim.api.nvim_buf_set_text(buf, row - 1, col - 1, row - 1, col - 1, vim.split(text, "\n"))
+end
+
+vim.api.nvim_create_user_command("Cheader", function(opts)
+  local the_file = (opts.fargs[1] or vim.fn.expand("%:t:r")):upper()
+  insert( 
+    '// #define '..the_file..'_IMPLEMENTATION\n'..
+    '#ifndef    _'..the_file..'_H_\n'..
+    '#define    _'..the_file..'_H_\n'..
+    '// '..the_file:lower()..' interface\n'..
+    '\n'..
+    '\n'..
+    '\n'..
+
+    '#ifdef      '..the_file..'_IMPLEMENTATION\n'..
+
+    '// '..the_file:lower()..' implementation\n'..
+    '\n'..
+    '\n'..
+    '\n'..
+
+    '#endif   // '..the_file..'_IMPLEMENTATION\n'..
+
+    '#endif   //_'..the_file..'_H_\n'..
+    ''
+
+)
+end, { nargs='?', desc = 'Generate generic C header into current buffer' })
